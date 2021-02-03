62823809f7c8a06d1bdfa7b11511015e25f66072
mchtylmz <35258432+mchtylmz@users.noreply.github.com>
2021-02-03 06:01:24 +0300

    Update app.php
    Yaş

--- app.php
+++ app.php
@@ -18,3 +18,16 @@ $age = 25;
  * 
  * İyi çalışmalar.
  */
+
+if($age <= 14) {
+ echo 'Çocuk';
+}
+elseif($age >= 15 && $age <= 24) {
+ echo 'Genç';
+} 
+elseif($age >= 25 && $age <= 64) {
+ echo 'Yetişkin';
+}
+else {
+ echo 'Yaşlı';
+}
